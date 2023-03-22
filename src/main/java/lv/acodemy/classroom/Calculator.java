458441d8dfdaf26ebbdb78bb8244c9d0d684cddb
onnaceslavska <o.ceslavska@gmail.com>
2023-03-22 15:36:32 +0200

    [classroom_9032023]: add progress

---
+++ src/main/java/lv/acodemy/classroom/Calculator.java
@@ -0,0 +1,24 @@
+package lv.acodemy.classroom;
+
+public class Calculator {
+
+    // sum, divide, multiply, subtract (4 methods)
+    // return int
+    // method should ask for 2 parameters
+
+    public int sum(int firstNumber, int secondNumber) {
+        return  firstNumber + secondNumber;
+    }
+    public int divide(int firstNumber, int secondNumber) {
+        return  firstNumber / secondNumber;
+    }
+
+    public int multiply(int firstNumber, int secondNumber) {
+
+        return  firstNumber * secondNumber;
+    }
+    public int subtract(int firstNumber, int secondNumber) {
+
+        return  firstNumber - secondNumber;
+    }
+}
---
+++ src/main/java/lv/acodemy/homework/SecondHomeWork.java
@@ -0,0 +1,7 @@
+package lv.acodemy.homework;
+
+public class SecondHomeWork {
+    public static void main(String[] args) {
+
+    }
+}
